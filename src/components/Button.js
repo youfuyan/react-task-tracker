ab607fe20f86e6394e2286f26a1336fbe29243f3
Youfu <arnoldyyf@gmail.com>
2022-06-15 19:56:18 -0400

    Task-Tracker

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import propTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button 
+    onClick={onClick} 
+    style={{background: color}} 
+    className='btn'>{text}</button>
+  )
+}
+
+Button.defaultProps = {
+  color: 'steelblue',
+}
+
+Button.propTypes = {
+  text: propTypes.string,
+  color:  propTypes.string,
+  onClick: propTypes.func,
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+
+
+  return (
+   <header className='header'>
+    <h1>{title}</h1>
+    <Button color={showAdd ? 'Red' : 'Green'} 
+    text={showAdd ? 'Close' : 'Add'} 
+    onClick={onAdd} 
+    />
+   </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//CSS IN JS
+// const headingStyle = {
+//     color: 'red', 
+//     background: 'black',
+// }
+
+export default Header
